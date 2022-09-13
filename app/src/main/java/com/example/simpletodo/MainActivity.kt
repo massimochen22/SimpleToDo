e5277956b31c42bc0278ffe2d7e33ccdcfc67954
Massimo Chen <haomassimochen@gmail.com>
2022-09-12 22:42:37 -0400

    First Commit

---
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.simpletodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import org.apache.commons.io.FileUtils.readLines
+import org.apache.commons.io.FileUtils.writeLines
+import java.io.File
+import java.io.IOException
+import java.nio.charset.Charset
+
+class MainActivity : AppCompatActivity() {
+    var listOfTasks = mutableListOf<String>()
+    lateinit var adapter: TaskItemAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val onLongClickListener = object : TaskItemAdapter.OnLongClickListener{
+            override fun onItemLongClicked(position: Int) {
+                listOfTasks.removeAt(position)
+                adapter.notifyDataSetChanged()
+                saveItems()
+            }
+
+        }
+//        findViewById<Button>(R.id.button).setOnClickListener(){
+//            Log.i("Massimo", "Clicked a button")
+//        }
+        listOfTasks.add("Do Laundry")
+        listOfTasks.add("Go for a walk")
+
+        loadItems()
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        adapter = TaskItemAdapter(listOfTasks, onLongClickListener)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val inputTextField = findViewById<EditText>(R.id.addTaskField)
+
+        findViewById<Button>(R.id.button).setOnClickListener{
+            val userInputtedTask = inputTextField.text.toString()
+            listOfTasks.add(userInputtedTask)
+
+            adapter.notifyItemInserted(listOfTasks.size-1)
+            inputTextField.setText("")
+            saveItems()
+        }
+    }
+
+    fun getDataFile():File{
+        return File(filesDir,"data.txt")
+    }
+
+    fun loadItems(){
+        try{
+            listOfTasks = readLines(getDataFile(), Charset.defaultCharset())
+        }
+        catch(ioException:IOException){
+            ioException.printStackTrace()
+        }
+    }
+
+    fun saveItems(){
+        try{
+            writeLines(getDataFile(),listOfTasks)
+        }
+        catch (ioException:IOException){
+            ioException.printStackTrace()
+        }
+
+    }
+}
